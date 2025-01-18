ba92918dc5e4d7a70234587f993f2be9202d582d
ThiagoRobade <robade@live.com>
2025-01-18 02:13:44 -0300

    Criando uma janela / Creating a window

--- pygame-produção-e-desenvolvimento-de-jogos/meu_jogo.py
+++ pygame-produção-e-desenvolvimento-de-jogos/meu_jogo.py
@@ -1,5 +1,25 @@
 import pygame
 
-# Inicia o pygame
+# Inicializa o Pygame
 pygame.init()
+
+# Configurações da janela
+largura, altura = 800, 600
+screen = pygame.display.set_mode((largura, altura))
+# A função set_mode() cria um objeto e tem como parâmetro uma tupla com dois inteiros,
+# neste exemplo se refere a largura e altura da janela.
+pygame.display.set_caption("Janela Simples")
+
+# Loop principal
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # Lógica do jogo aqui
+
+    # Renderização aqui
+
+    pygame.display.update()
 
